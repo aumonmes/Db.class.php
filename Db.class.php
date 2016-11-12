2a9b459ccedb8839be2e813409ab282659ec9020
aumon <aumonmes@gmail.com>
2016-11-12 19:25:50 +0000

    Fixed some bugs Methods reorganized Variables renamed for better reading Transaction's error messages improved for clarity

--- Db.class.php
+++ Db.class.php
@@ -5,151 +5,145 @@ class Db extends mysqli{
 	private $transaction_error = 0;
 	public $transaction_msg= [];
 
-	/* Execute a query with no return */
-	public function exec($sql){
-		if($this->query($sql)) return $this->affected_rows;
-		else{
-			if($transaction_flag) $this->trans_error();
-			return false;
-		}
-	}
+	/**
+	**	Data selection
+	**/
 
-	/* Return only first row of a query returning null for an empty result */
-	public function query_one($str){
-		if($r = $this->query($str)){
-			$res = $r->fetch_assoc();
-			if(isset($res)) return $res;
-			else return null;
-		}
-		else{
-			if($transaction_flag) $this->trans_error();
-			return false;
-		}
-	}
-
-	/* Return only one value for a query, if there's more than one field returns the first one */
-	public function query_val($str){
-		$r = null;
-		if($res = $this->query_one($str)){
-			if(is_array($res)) $r = array_shift($res);
-			return $r;
-		}else return false;
-	}
-
-	/* Return a query's results as an array instead as a mysqli object */
-	public function query_arr($str){
-		$res = [];
-		if($r = $this->query($str)){
-			while($r && $t = $r->fetch_assoc()) $res[] = $t;
-			return $res;
+	/* Return a query's results as an array instead of a mysqli object */
+	public function query_arr($query){
+		$return = [];
+		if($rows = $this->query($query)){
+			while($rows && $r = $rows->fetch_assoc()) $return[] = $r;
+			return $return;
 		}else return false;
 	}
 
 	/* Returns a non associative array with the values of the first column of the result */
 	public function query_col($str){
-		$res = [];
-
-		if($r = $this->query($str)){
-			while($r && $t = $r->fetch_row()) $res[] = $t[0];
-			return $res;
+		$return = [];
+		if($rows = $this->query($str)){
+			while($rows && $r = $rows->fetch_row()) $return[] = $r[0];
+			return $return;
 		}else{
-			if($transaction_flag) $this->trans_error();
+			if($this->transaction_flag) $this->trans_error();
 			return false;
 		}
+	}
 
+	/* Return only first row of a query as associative array returning null for an empty result */
+	public function query_one($query){
+		if($rows = $this->query($query)){
+			$return = $rows->fetch_assoc();
+			if(isset($return)) return $return;
+			else return null;
+		}else{
+			if($this->transaction_flag) $this->trans_error();
+			return false;
+		}
+	}
+
+	/* Return only one value for a query, if there's more than one field returns the first one */
+	public function query_val($query){
+		$return = null;
+		if($row = $this->query_one($query)){
+			if(is_array($row)) $return = array_shift($row);
+			return $return;
+		}else return false;
+	}
+
+
+	/**
+	**	Data manipulation
+	**/
+
+	/* Execute a query returning number of affected rows, it might return int(0) therefore use === when checking it's result */
+	public function exec($query){
+		if($this->query($query)) return $this->affected_rows;
+		else{
+			if($this->transaction_flag) $this->trans_error();
+			return false;
+		}
 	}
 
 	/* Insert one single row using an associative array */
-	public function insert($table, $values){
-		if(empty($values)) return false;
+	public function insert($table, $data){
+		if(empty($data)) return false;
 
 		$keys = [];
-		$vals = [];
+		$values = [];
+		$table = $this->sanitize($table);
 
-		foreach($values as $k => $v){
-			$keys[] = $k;
-			$vals[] = "'".$this->sanitize($v)."'";
+		foreach($data as $key => $val){
+			$keys[] = $key;
+			$values[] = "'".$this->sanitize($val)."'";
 		}
 
 		$sql = "INSERT INTO $table
-			(".implode(",", $keys).")
-			VALUES
-			(".implode(",", $vals).")";
+				(".implode(",", $keys).")
+				VALUES
+				(".implode(",", $values).")";
 		return $this->exec($sql);
 	}
 
-	/* Executes a batch of queries in one array @@@ REDO
-	public function multi_exec($sql){
-		$ret = [
-			"e" => false,
-			"msg" => []
-		];
-		if($this->multi_query($sql)){
-			do{
-				d($this);
-				if($this->errno !== 0){
-					$ret['e'] = true;
-					$ret['msg'][] = $this->error;
-				}
-			}while($this->next_result());
-		}
 
-		return $ret;
-	}
-	*/
-
-	/** Transaction related methods **/
+	/**
+	**	Transaction related methods
+	**/
 
 	/* Start a new transaction */
 	public function trans_start(){
 		if(!$this->transaction_flag){
 			$this->transaction_error = 0;
 			$this->transaction_msg = [];
-			if($this->exec("STRART TRANSACTION")){
+			if($this->exec("START TRANSACTION") !== false){
 				$this->transaction_flag = true;
 				return true;
 			}else{
-				$this->trans_error("TRANSACTION couldn't be started");
+				$this->trans_error("Error when starting transaction");
 				return false;
 			}
 		}else{
-			$this->trans_error("There's already a transaction started");
+			$this->trans_error("One transaction is already running");
 			return false;
 		}
 	}
 
-	/* Finish a transaction */
+	/* End a transaction */
 	public function trans_end(){
+		if(!$this->transaction_flag){
+			$this->trans_error("No transaction to end");
+			return false;
+		}
+
 		$commit = false;
-		if($this->transaction_error > 0) $db->exec("ROLLBACK");
+		if($this->transaction_error > 0) $this->exec("ROLLBACK");
 		else{
-			$db->exec("COMMIT");
+			$this->exec("COMMIT");
 			$commit = true;
 		}
 		$this->transaction_flag = false;
-		$this->transaction_error = 0;
-		$this->transaction_msg = [];
-
 		return $commit;
 	}
 
-	/* Manage transaction's error message */
+	/* Manage transaction's errors */
 	private function trans_error($str = false){
 		$str = $str ? $str : $this->error;
 		$this->transaction_error++;
 		$this->transaction_msg[] = $str;
 	}
 
+	/**
+	**	Utils
+	**/
+
 	/* Escapes strings and removes slashes if possible */
 	public function sanitize($str){
 		if(is_array($str)){
-			foreach($str as $k => $val) $output[$k] = $this->sanitize($val);
+			foreach($str as $key => $val) $output[$key] = $this->sanitize($val);
 		}else{
 			if(get_magic_quotes_gpc()) $str = stripslashes($str);
 			$output = $this->real_escape_string($str);
 		}
 		return $output;
 	}
-
 }
-?>
